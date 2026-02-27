deef8154dd25c91f792c3494e29c50d2fd6a7f01
attackufeiz <attackufeiz@gmail.com>
2026-02-27 15:55:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -42,11 +42,11 @@ MY_CHANNEL = "@attackufeiz"
 
 URLS_RU = [
     "https://raw.githubusercontent.com/AvenCores/goida-vpn-configs/refs/heads/main/githubmirror/26.txt"
-    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt"
+    
 ]
 
 URLS_MY = [
-    
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt"
 ]
 
 EURO_CODES = {"NL", "DE", "FI", "GB", "FR", "SE", "PL", "CZ", "AT", "CH", "IT", "ES", "NO", "DK", "BE", "IE", "LU", "EE", "LV", "LT"}
@@ -415,6 +415,7 @@ if __name__ == "__main__":
     print("\n✅ SUCCESS: FAST/ALL LAYERS GENERATED")
     print(f"  Префильтр: {len(RU_FILES)} RU + {len(EURO_FILES)} EURO (FAST)")
     print(f"  Постер: до 8 кнопок (FAST + ограниченные ALL)")
+
 
 
 
