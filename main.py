539cb4375b25c88efc86d9d8b081b47652bff57c
attackufeiz <attackufeiz@gmail.com>
2026-03-17 19:51:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -58,11 +58,12 @@ URLS_RU = [
     "https://etoneya.a9fm.site/1",
     "https://s3c3.001.gpucloud.ru/vahe4xkwi/cjdr"
     "https://raw.githubusercontent.com/kort0881/vpn-vless-configs-russia/refs/heads/main/githubmirror/new/all_new.txt"
-    "https://raw.githubusercontent.com/AvenCores/goida-vpn-configs/refs/heads/main/githubmirror/26.txt"
 ]
 
 URLS_MY = [
     "https://raw.githubusercontent.com/kort0881/vpn-vless-configs-russia/refs/heads/main/githubmirror/new/all_new.txt"
+    "https://raw.githubusercontent.com/AvenCores/goida-vpn-configs/refs/heads/main/githubmirror/26.txt"
+    
 ]
 
 EURO_CODES = {
