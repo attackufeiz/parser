d359f8c2bb3f03dab85219bab812b5829baf944b
attackufeiz <attackufeiz@gmail.com>
2026-03-17 19:47:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ RU_FILES = ["ru_white_part1.txt", "ru_white_part2.txt", "ru_white_part3.txt", "r
 EURO_FILES = ["my_euro_part1.txt", "my_euro_part2.txt", "my_euro_part3.txt"]
 
 HISTORY_FILE = os.path.join(BASE_DIR, "history.json")
-MY_CHANNEL = "@vlesstrojan"
+MY_CHANNEL = "@attackufeiz"
 
 URLS_RU = [
     "https://github.com/igareck/vpn-configs-for-russia/blob/main/BLACK_VLESS_RUS_mobile.txt",
